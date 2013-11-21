62279cb47fe173f6670e96ce86a2ba9f834ab8c7
pvamshi <pvamshi@gmail.com>
2013-11-21 09:09:34 +0530

    Explicit python 2.7

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 command -v virtualenv >/dev/null 2>&1 || { echo >&2 "I require foo but it's not installed.  Aborting."; exit 1; }
-virtualenv venv
+virtualenv -p python2.7 venv
 . ./venv/bin/activate
 pip install -r requirements.txt
